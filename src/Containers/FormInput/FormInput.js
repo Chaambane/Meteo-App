e5d61a834eab56d5678ff02c8e2b13001d1c548d
FAIDINE <mohamed.faidine@live.fr>
2022-01-17 09:53:47 +0100

    refact input width and add access file

--- src/Containers/FormInput/FormInput.js
+++ src/Containers/FormInput/FormInput.js
@@ -16,8 +16,8 @@ class FormInput extends Component {
         return (
             <div className="mt-4 title">
                 <input type="text" 
-                    className="form-control w-50 p-4 text-center fw-bold rounded-pill fw-bold fs-2" 
-                    placeholder="Saisissez une ville"
+                    className="d-flex form-control w-50 p-4 text-center fw-bold rounded-pill fw-bold fs-2" 
+                    placeholder="Ville"
                     onChange={(e) => this.setState({cityName: e.target.value})}
                     value={this.state.cityName}
                     onKeyPress={this.handleSubmitCity}
--- src/Containers/Weather/Temp/DetailsTemp/DetailsTemp.js
+++ src/Containers/Weather/Temp/DetailsTemp/DetailsTemp.js
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-    const detailsTemp = (props) => (
-        <div className="container temp w-50 detailsTemp rounded">
-            <div className="container d-flex mt-4">
-                <div className="col-6">
-                    <p>Température Ressentie: <span className="fs-4">{props.main.feels_like}°</span></p>
-                    <p>Température maximale: <span className="fs-4">{props.main.temp_max}°</span></p>
-                    <p>Température minimale: <span className="fs-4">{props.main.temp_min}°</span></p>
-                </div>
-                <div className="col-6">
-                    <p>Vent: <span className="fs-4">{props.wind.speed} Km/h</span></p>
-                    <p>Humidité: <span className="fs-4">{props.main.humidity}</span></p>
-                    <p>Pression: <span className="fs-4">{props.main.pressure} Pa</span></p>
-                </div>
+const detailsTemp = (props) => (
+    <div className="container temp w-50 detailsTemp rounded">
+        <div className="container d-flex mt-4">
+            <div className="col-6">
+                <p>Température Ressentie: <span className="fs-4">{props.main.feels_like}°</span></p>
+                <p>Température maximale: <span className="fs-4">{props.main.temp_max}°</span></p>
+                <p>Température minimale: <span className="fs-4">{props.main.temp_min}°</span></p>
+            </div>
+            <div className="col-6 row">
+                <p>Vent: <span className="fs-4 col-sm">{props.wind.speed} Km/h</span></p>
+                <p>Humidité: <span className="fs-4 col-sm">{props.main.humidity}</span></p>
+                <p>Pression: <span className="fs-4 col-sm">{props.main.pressure} Pa</span></p>
             </div>
         </div>
-    );
+    </div>
+);
 
 export default detailsTemp;
